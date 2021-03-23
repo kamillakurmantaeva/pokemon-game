3bf5a82bc3ab9d9d760adc4d30ff5b551d7f4761
Kamilla <kamilla.danielevna@gmail.com>
2021-03-23 20:50:29 +0300

    created database

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,44 +1,63 @@
 import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
 import PokemonCard from '../../components/PokemonCard';
 import database from '../../service/firebase';
 import s from './style.module.css';
 
+const DATA = { id: 12345 };
+
 const GamePage = () => {
   const [pokemons, setPokemons] = useState({});
 
-  useEffect(() => {
+  const getPokemons = () => {
     database.ref('pokemons').once('value', (snapshot) => {
       setPokemons(snapshot.val());
     });
-  }, []);
-
-  const setActiveDatabase = (id) => {
-    database.ref('pokemons/' + id).set({
-      active: !pokemons[id].active,
-    });
   };
 
-  const setActive = (id) => {
+  useEffect(() => getPokemons(), []);
+
+  const setActiveDatabase = (key, id) => {
+    database
+      .ref('pokemons/' + key)
+      .update({
+        active: !pokemons[id].active,
+      })
+      .then(() =>
+        setPokemons((prevState) => {
+          const state = { ...prevState };
+          state[key] = { ...state[key], active: !state[key].active };
+          return state;
+        })
+      );
+  };
+
+  const setActive = (key, id) => {
     setPokemons((prevState) => {
       return Object.entries(prevState).reduce((acc, item) => {
         const pokemon = { ...item[1] };
         if (pokemon.id === id) {
-          pokemon.active = true;
-          setActiveDatabase(id);
+          pokemon.active = !pokemon.active;
+          setActiveDatabase(key, id);
         }
         acc[item[0]] = pokemon;
+        database.ref('pokemons/' + item[0]).set(pokemon);
         return acc;
       }, {});
     });
   };
 
-  const history = useHistory();
-  const handleClick = () => history.push('/');
+  const handleClick = () => {
+    const data = DATA;
+    const newKey = database.ref().child('pokemons').push().key;
+    database
+      .ref('pokemons/' + newKey)
+      .set(data)
+      .then(() => getPokemons());
+  };
 
   return (
     <div>
-      <button onClick={handleClick}>Back</button>
+      <button onClick={handleClick}>Add new pokemon</button>
       <div className={s.flex}>
         {Object.entries(pokemons).map(
           ([key, { name, img, id, type, values, active }]) => (
@@ -50,7 +69,7 @@ const GamePage = () => {
               type={type}
               values={values}
               isActive={active}
-              setActive={() => setActive(id, key)}
+              setActive={setActive}
             />
           )
         )}
